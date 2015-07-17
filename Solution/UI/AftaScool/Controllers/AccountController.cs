ed1e8445db73fb311bd2cb12556883ea40478a64
OlusegunTcr <olusegun@thecoderoom.co.za>
2015-07-17 10:04:14 +0200

    UI files added

---
+++ Solution/UI/AftaScool/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System;
+using System.Net;
+using AftaScool.Models.Account;
+using AftaScool.BL.Context;
+using AftaScool.BL.Provider.Security;
+
+namespace AftaScool.Controllers
+{
+    [Authorize]
+    public class AccountController : TCRControllerBase
+    {
+        private DataContext Context { get; set; }
+        private bool _DisposeContext = false;
+
+        private ISecurityProvider SecurityProvider { get; set; }
+
+        public AccountController()
+        {
+            Context = new DataContext();
+            _DisposeContext = true;
+            SecurityProvider = new SecurityProvider(Context,CurrentUser);
+        }
+
+        public AccountController(ISecurityProvider securityProvider)
+        {
+            SecurityProvider = securityProvider;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_DisposeContext)
+                Context.Dispose();
+
+            base.Dispose(disposing);
+
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Error details");
+
+                var user = SecurityProvider.UserLogin(model.UserName, model.Password);
+                CurrentUser = new Models.Account.CurrentUserModel(user);
+
+                FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+
+                return SerializeToAngular(CurrentUser);
+
+            }
+            catch (SecurityException e)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    var user = SecurityProvider.SignUp(model.UserName, model.Password, model.Email,
+                        model.Title, model.FirstName, model.Surname,
+                        model.IDOrPassport, model.GenderType, model.Telephone,
+                        model.AddressLine1, model.AddressLine2, model.City,
+                        model.PostalCode);
+
+                    var aCurrentUser = SecurityProvider.UserIdentityToCurrentUser(user);
+                    CurrentUser = new Models.Account.CurrentUserModel(aCurrentUser);
+
+                    FormsAuthentication.SetAuthCookie(model.Email, false);
+
+                    return SerializeToAngular(CurrentUser);
+
+                }
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError,"Validation Error");
+            }
+            catch (SecurityException e)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
